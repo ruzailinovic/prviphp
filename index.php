7353875012367f3cb3264962d562d109260fa4b8
Ruža Ilinović <ruza.ilinovic@skole.hr>
2016-03-09 13:36:19 +0100

    Dodali smo PHP kod i varijable.

--- index.php
+++ index.php
@@ -2,10 +2,26 @@
 <html>
 <head>
     <title>Prvi PHP</title>
+    <meta charset="utf-8"
 </head>
 
 <body>
-<h1>Prvi PHP</h1>
+<h1>Ruža je najjj</h1>
+
+<?php
+    echo('Ovo je prije odlomka<br>');
+    echo('<br>');
+    echo('Super, jos cemo dodati<br>');
+    echo date ('d.m.Y');
+
+    $ocjena=5;
+    echo('<br>Vaša ocjena je: ' . $ocjena);
+
+    $ocjena=1;
+    echo('<br>Vaša slijedeća ocjena je: ' . $ocjena);
+
+
+?>
 
 <p>
     Ovo je odlomak
